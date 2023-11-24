016cc0e27fff205922cb75b9e0392d9f5869e986
Arthur Pontes Nader <82470629+arthurnader@users.noreply.github.com>
2023-11-24 18:54:34 -0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+import math
+
+def fatorial(n):
+    return math.factorial(n)
+
+def exponenciacao(base, expoente):
+    return base ** expoente
+
+def logaritmo(numero, base):
+    return math.log(numero, base)
